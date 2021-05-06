2993364699abe88ee1d07680b8c44a1c98b792d6
gaotianjun <44943100+gaotianjun@users.noreply.github.com>
2021-05-06 18:20:46 +0800

    Add files via upload
    add js防止缓存

--- js/article.js
+++ js/article.js
@@ -1,15 +1,15 @@
-function Loading(){
-    var str = location.search.substring(4);
-    var number = Number(str);
-    for (let i = 0; i < datas.length; i++) {
-        if (datas[i].id == number) {
-            var code = `<h2>${datas[i].title}</h2>
+
+var str = location.search.substring(4);
+var number = Number(str);
+for (let i = 0; i < datas.length; i++) {
+    if (datas[i].id == number) {
+        var code = `<h2>${datas[i].title}</h2>
             <p class='time'>${datas[i].time}</p>
             <hr style=" height:6px;border:none;border-top:1px solid #185598;" />
             <img src=${datas[i].src} alt='资讯图'  class='imgs'/>
             <p>${datas[i].content}</p>`;
-            $('#content').html(code);
-        }
+        $('#content').html(code);
     }
 }
+
 
